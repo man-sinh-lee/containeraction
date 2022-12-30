81b70dd5eb75e81099cc6f806254b0daa94c3bfd
Man.Sinh.Lee <114969060+man-sinh-lee@users.noreply.github.com>
2022-12-30 10:08:25 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,8 @@
 
 echo "Hello $INPUT_MYINPUT"
 memory=$(cat /proc/meminfo)
+disk_usage=$(df -hPT)
+list_most_memory_consumption=$(ps aux --sort -rss |head)
+echo "::set-output name=disk_usage::$disk_usage"
 echo "::set-output name=memory::$memory"
+echo "::set-output name=list_most_memory_consumption::$list_most_memory_consumption"
